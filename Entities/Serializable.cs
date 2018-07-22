e78cb91d6e749b1a2dd1c16a80b281c2fa7dda2e
RoySubhojit <subhojitroy89@gmail.com>
2018-07-22 19:54:55 +0530

    Add few required projects

---
+++ Entities/Serializable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Entities
+{
+    public class Serializable<T>
+    {
+        public string Serialize()
+        {
+            string retStr = string.Empty;
+            var obj = this;
+            if (obj != null)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                using (StringWriter writter = new StringWriter())
+                {
+                    serializer.Serialize(writter, obj);
+                    retStr = writter.ToString();
+                }
+            }
+            return retStr;
+        }
+
+        public T Parse(string str)
+        {
+            T obj = default(T);
+            if (!string.IsNullOrEmpty(str))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                using (StringReader reader = new StringReader(str))
+                {
+                    obj = (T)serializer.Deserialize(reader);
+                }
+            }
+            return obj;
+        }
+    }
+}
