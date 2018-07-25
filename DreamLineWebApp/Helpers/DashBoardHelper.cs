9b0a15f4711a81ecdf07b2bb430e44d6deb8aaef
RoySubhojit <subhojitroy89@gmail.com>
2018-07-26 02:29:32 +0530

    Initial Commit

---
+++ DreamLineWebApp/Helpers/DashBoardHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using nBUSINESS = BusinessLogic;
+using nENTITIES = Entities;
+using nCOMMON = Common;
+using DreamLineWebApp.Models;
+
+namespace DreamLineWebApp.Helpers
+{
+    public class DashBoardHelper
+    {
+        public DashBoard GetDashBoard()
+        {
+            DashBoard dashBoard = null;
+            nBUSINESS.DashBoardHelper helper = new nBUSINESS.DashBoardHelper();
+            return dashBoard;
+        }
+    }
+}
---
+++ DreamLineWebApp/Models/Products.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DreamLineWebApp.Models
+{
+    public class Products
+    {
+    }
+}
